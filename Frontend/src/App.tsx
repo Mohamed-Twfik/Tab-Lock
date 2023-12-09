c670482714dfeb8088162d9e02c8eada9d17b8d2
zikaloai71 <ze.lo.moukhtar@el-eng.menofia.edu.eg>
2023-12-09 11:07:09 +0200

    intial setup for frontend with react and vite

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import Routes from "./routes"
+
+
+const queryClient = new QueryClient()
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+  )
+}
+
+export default App
